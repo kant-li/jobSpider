9b3043bf0fd6b8439f870bf7990002ce14c48941
kant <lirongxin14@qq.com>
2017-11-17 17:52:41 +0800

    by language

--- jobSpider.py
+++ jobSpider.py
@@ -8,7 +8,15 @@ from bs4 import BeautifulSoup
 import re
 import csv
 
-url = 'http://sou.zhaopin.com/jobs/searchresult.ashx?bj=160000&sj=2039&jl=%E7%A6%8F%E5%BB%BA&sm=0&p='
+# url = 'http://sou.zhaopin.com/jobs/searchresult.ashx?bj=160000&sj=2040&jl=%E7%A6%8F%E5%B7%9E&p='    ##java
+# url = 'http://sou.zhaopin.com/jobs/searchresult.ashx?bj=160000&sj=2039&jl=%E7%A6%8F%E5%B7%9E&p='    ##Android
+# url = 'http://sou.zhaopin.com/jobs/searchresult.ashx?bj=160000&sj=2041&jl=%E7%A6%8F%E5%B7%9E&p='    ##php
+# url = 'http://sou.zhaopin.com/jobs/searchresult.ashx?bj=160000&sj=864&jl=%E7%A6%8F%E5%B7%9E&p='    ##web前端
+# url = 'http://sou.zhaopin.com/jobs/searchresult.ashx?bj=160000&sj=2038&jl=%E7%A6%8F%E5%B7%9E&p='    ##IOS
+url = 'http://sou.zhaopin.com/jobs/searchresult.ashx?bj=160000&sj=2042&jl=%E7%A6%8F%E5%B7%9E&p='    ##C
+
+
+
 headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36'}
 
 def jobFind(url, headers):
@@ -16,15 +24,40 @@ def jobFind(url, headers):
     req = Request(url=url, headers=headers)
     response = urlopen(req)
     soup = BeautifulSoup(response, 'html.parser')
-    result = soup.find_all(class_= 'zwmc')
-    for jobDescribe in result:
-        pattern = re.compile(r'http://jobs\.zhaopin\.com/.+\.htm')
-        jobLink = pattern.search(str(jobDescribe))
-        if jobLink is not None:
-            print(jobLink.group())
-            jobInfo(url=jobLink.group(), headers=headers)
+
+    ##针对公司寻找名称、链接
+    result = soup.find_all(class_= 'gsmc')
+    for companyDescribe in result:
+        ###名称
+        pattern1 = re.compile(r'k">.+公司')
+        companyName = pattern1.search(str(companyDescribe))
+        data = []
+        if companyName is not None:
+            companyNameClean = companyName.group()[3:]
+            print(companyNameClean)
+            data.append(companyNameClean)
+        ###针对公司寻找链接
+        pattern2 = re.compile(r'http://company\.zhaopin\.com/.+\.htm')
+        companyDescribe2 = str(companyDescribe)[:80]
+        # print(companyDescribe2)
+        companyLink = pattern2.search(companyDescribe2)
+        if companyLink is not None:
+            print(companyLink.group())
+            data.append(companyLink.group())
+            companyInfo(url=companyLink.group(), headers=headers, data=data)
+
+
+    ##针对职务寻找链接
+    # result = soup.find_all(class_= 'zwmc')
+    # for jobDescribe in result:
+    #     pattern = re.compile(r'http://jobs\.zhaopin\.com/.+\.htm')
+    #     jobLink = pattern.search(str(jobDescribe))
+    #     if jobLink is not None:
+    #         print(jobLink.group())
+    #         jobInfo(url=jobLink.group(), headers=headers)
 
 def jobInfo(url, headers):
+    '''根据岗位链接获取具体信息，并写入表格；'''
     req = Request(url=url, headers=headers)
     response = urlopen(req)
     soup = BeautifulSoup(response, 'html.parser')
@@ -39,16 +72,25 @@ def jobInfo(url, headers):
         text = t.strip().replace('\n', ' ')
         if text != '':
             data.append(text)
-    print(data)
+    # print(data)
     writer.writerow(data)
 
-
-def jobInfoStrip(jobinfo):
-    pass
+def companyInfo(url, headers, data):
+    '''根据公司链接获取具体信息，并写入表格；'''
+    req = Request(url=url, headers=headers)
+    response = urlopen(req)
+    soup = BeautifulSoup(response, 'html.parser')
+    # print(soup)
+    result = soup.find(class_= 'comAddress').strings
+    for i in result:
+        text = i.strip().replace('\n', ' ')
+        if text != '':
+            data.append(text)
+    writer.writerow(data)
 
 csvfile = open('csv_test.csv', 'w', encoding='utf-8-sig')
 writer = csv.writer(csvfile)
-for p in range(1,5):
+for p in range(1,2):
     url1 = url + str(p)
     print('-' * 20 + str(p))
     print(url1)
