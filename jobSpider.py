f761eca8964e765689dc9557c884db45b41d2ff2
kant <lirongxin14@qq.com>
2017-12-06 16:50:27 +0800

    some style fix

--- jobSpider.py
+++ jobSpider.py
@@ -7,22 +7,30 @@ from urllib.request import Request
 from bs4 import BeautifulSoup
 import re
 import csv
+import time
 
-url1 = 'http://sou.zhaopin.com/jobs/searchresult.ashx?bj=160000&sj=2040&jl=%E7%A6%8F%E5%B7%9E&p='    ##java
-url2 = 'http://sou.zhaopin.com/jobs/searchresult.ashx?bj=160000&sj=2039&jl=%E7%A6%8F%E5%B7%9E&p='    ##Android
-url3 = 'http://sou.zhaopin.com/jobs/searchresult.ashx?bj=160000&sj=2041&jl=%E7%A6%8F%E5%B7%9E&p='    ##php
-url4 = 'http://sou.zhaopin.com/jobs/searchresult.ashx?bj=160000&sj=864&jl=%E7%A6%8F%E5%B7%9E&p='    ##web前端
-url5 = 'http://sou.zhaopin.com/jobs/searchresult.ashx?bj=160000&sj=2038&jl=%E7%A6%8F%E5%B7%9E&p='    ##IOS
-url6 = 'http://sou.zhaopin.com/jobs/searchresult.ashx?bj=160000&sj=2042&jl=%E7%A6%8F%E5%B7%9E&p='    ##C
-url7 = 'http://sou.zhaopin.com/jobs/searchresult.ashx?jl=%E7%A6%8F%E5%B7%9E&'\
-        + 'kw=.net&isadv=0&isfilter=1&sg=3ed3b548f6cd4b41a75bb6f6cfe995d3&p='    ##.net
+# url1 = 'http://sou.zhaopin.com/jobs/searchresult.ashx?bj=160000&sj=2040&jl=%E7%A6%8F%E5%B7%9E&p='    ##java
+# url2 = 'http://sou.zhaopin.com/jobs/searchresult.ashx?bj=160000&sj=2039&jl=%E7%A6%8F%E5%B7%9E&p='    ##Android
+# url3 = 'http://sou.zhaopin.com/jobs/searchresult.ashx?bj=160000&sj=2041&jl=%E7%A6%8F%E5%B7%9E&p='    ##php
+# url4 = 'http://sou.zhaopin.com/jobs/searchresult.ashx?bj=160000&sj=864&jl=%E7%A6%8F%E5%B7%9E&p='    ##web前端
+# url5 = 'http://sou.zhaopin.com/jobs/searchresult.ashx?bj=160000&sj=2038&jl=%E7%A6%8F%E5%B7%9E&p='    ##IOS
+# url6 = 'http://sou.zhaopin.com/jobs/searchresult.ashx?bj=160000&sj=2042&jl=%E7%A6%8F%E5%B7%9E&p='    ##C
+# url7 = 'http://sou.zhaopin.com/jobs/searchresult.ashx?jl=%E7%A6%8F%E5%B7%9E&'\
+#         + 'kw=.net&isadv=0&isfilter=1&sg=3ed3b548f6cd4b41a75bb6f6cfe995d3&p='    ##.net
 
-urlList = [url1, url2, url3, url4, url5, url6, url7]
+urlDict = {'java':'http://sou.zhaopin.com/jobs/searchresult.ashx?bj=160000&sj=2040&jl=%E7%A6%8F%E5%B7%9E&p=',\
+            'android':'http://sou.zhaopin.com/jobs/searchresult.ashx?bj=160000&sj=2039&jl=%E7%A6%8F%E5%B7%9E&p=',\
+            'php':'http://sou.zhaopin.com/jobs/searchresult.ashx?bj=160000&sj=2041&jl=%E7%A6%8F%E5%B7%9E&p=',\
+            'web前端':'http://sou.zhaopin.com/jobs/searchresult.ashx?bj=160000&sj=864&jl=%E7%A6%8F%E5%B7%9E&p=',\
+            'ios':'http://sou.zhaopin.com/jobs/searchresult.ashx?bj=160000&sj=2038&jl=%E7%A6%8F%E5%B7%9E&p=',\
+            'C':'http://sou.zhaopin.com/jobs/searchresult.ashx?bj=160000&sj=2042&jl=%E7%A6%8F%E5%B7%9E&p=',\
+            '.net':'http://sou.zhaopin.com/jobs/searchresult.ashx?jl=%E7%A6%8F%E5%B7%9E&'\
+                    + 'kw=.net&isadv=0&isfilter=1&sg=3ed3b548f6cd4b41a75bb6f6cfe995d3&p='}
 
 headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3)\
             AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36'}
 
-def jobFind(url, headers):
+def jobFind(keyWord, cmpList, url, headers):
     ##爬区网页
     # req = Request(url=quote(url, safe='/:?='), headers=headers)
     req = Request(url=url, headers=headers)
@@ -33,23 +41,28 @@ def jobFind(url, headers):
     result = soup.find_all(class_= 'gsmc')
     cmpCount = len(result)      #用于统计页面中的公司总数，如果低于某个设定数值，则到了末页，本值需要返回
     for companyDescribe in result:
+        time.sleep(0.1)
         ###名称
         pattern1 = re.compile(r'k">.+公司')
         companyName = pattern1.search(str(companyDescribe))
-        data = []
+        data = [keyWord]
         if companyName is not None:
             companyNameClean = companyName.group()[3:]
-            print(companyNameClean)
-            data.append(companyNameClean)
-        ###针对公司寻找链接
-        pattern2 = re.compile(r'http://company\.zhaopin\.com/.+\.htm')
-        companyDescribe2 = str(companyDescribe)[:80]
-        # print(companyDescribe2)
-        companyLink = pattern2.search(companyDescribe2)
-        if companyLink is not None:
-            print(companyLink.group())
-            data.append(companyLink.group())
-            companyInfo(url=companyLink.group(), headers=headers, data=data)
+            if companyNameClean not in cmpList:
+                print(companyNameClean)
+                cmpList.append(companyNameClean)
+                data.append(companyNameClean)
+                ###针对公司寻找链接
+                pattern2 = re.compile(r'http://company\.zhaopin\.com/.+\.htm')
+                companyDescribe2 = str(companyDescribe)[:80]
+                # print(companyDescribe2)
+                companyLink = pattern2.search(companyDescribe2)
+                if companyLink is not None:
+                    print(companyLink.group())
+                    data.append(companyLink.group())
+                    companyInfo(url=companyLink.group(), headers=headers, data=data)
+        else:
+            continue
     return cmpCount
 
 
@@ -95,15 +108,18 @@ def companyInfo(url, headers, data):
     writer.writerow(data)
 
 #主流程
-csvfile = open('csv_test.csv', 'w', encoding='utf-8-sig')
+csvfile = open('csv_test.csv', 'w', encoding='utf-8-sig', newline='')
 writer = csv.writer(csvfile)
-for url in urlList:
+titles = ["岗位类别","公司名称","链接","地址"]
+writer.writerow(titles)
+for key in urlDict.keys():
     p = 0
-    cmpInPage = 20      #页面内的公司数，用于判定是否末页
+    cmpInPage = 20      # 页面内的公司数，用于判定是否末页
+    cmpList = []        # 同一个岗位已收集的公司名称，避免重复收集数据
     while cmpInPage > 10:   #如果页面内公司数少于10，则判定为末页，否则继续下一页
         p += 1
-        url1 = url + str(p)
-        print('-' * 20 + str(p))
+        url1 = urlDict[key] + str(p)
+        print(key + '-' * 20 + str(p))
         print(url1)
-        cmpInPage = jobFind(url1, headers)
+        cmpInPage = jobFind(key, cmpList, url1, headers)
 csvfile.close()
