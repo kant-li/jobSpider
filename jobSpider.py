93559568bbd4b99a165460acb0c1fa6b90c5ada8
kant <lirongxin14@qq.com>
2017-12-27 14:38:15 +0800

    取消工商信息查询

--- jobSpider.py
+++ jobSpider.py
@@ -4,24 +4,25 @@
 from urllib.request import urlopen
 from urllib.parse import quote
 from urllib.request import Request
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup       # pip install bs4安装，网页解析包，很常用
 import re
 import csv
 import time
-import geetest
+# import geetest        # 破解滑动验证码，暂时不用了
 
-GSXT_HOST_FJ = 'http://fj.gsxt.gov.cn'
-GSXT_INDEX_FJ = GSXT_HOST_FJ + '/notice/'
+# 用于爬取工商信息的两行参数，暂停使用
+# GSXT_HOST_FJ = 'http://fj.gsxt.gov.cn'
+# GSXT_INDEX_FJ = GSXT_HOST_FJ + '/notice/'
 
-# urlDict = {'java':'http://sou.zhaopin.com/jobs/searchresult.ashx?bj=160000&sj=2040&jl=%E7%A6%8F%E5%B7%9E&p=',\
-#             'android':'http://sou.zhaopin.com/jobs/searchresult.ashx?bj=160000&sj=2039&jl=%E7%A6%8F%E5%B7%9E&p=',\
-#             'php':'http://sou.zhaopin.com/jobs/searchresult.ashx?bj=160000&sj=2041&jl=%E7%A6%8F%E5%B7%9E&p=',\
-#             'web前端':'http://sou.zhaopin.com/jobs/searchresult.ashx?bj=160000&sj=864&jl=%E7%A6%8F%E5%B7%9E&p=',\
-#             'ios':'http://sou.zhaopin.com/jobs/searchresult.ashx?bj=160000&sj=2038&jl=%E7%A6%8F%E5%B7%9E&p=',\
-#             'C':'http://sou.zhaopin.com/jobs/searchresult.ashx?bj=160000&sj=2042&jl=%E7%A6%8F%E5%B7%9E&p=',\
-#             '.net':'http://sou.zhaopin.com/jobs/searchresult.ashx?jl=%E7%A6%8F%E5%B7%9E&'\
-#                     + 'kw=.net&isadv=0&isfilter=1&sg=3ed3b548f6cd4b41a75bb6f6cfe995d3&p='}
-urlDict = {'java':'http://sou.zhaopin.com/jobs/searchresult.ashx?bj=160000&sj=2040&jl=%E7%A6%8F%E5%B7%9E&p='}
+# 字典，每个岗位对应的搜索网址，去掉page值
+urlDict = {'java':'http://sou.zhaopin.com/jobs/searchresult.ashx?bj=160000&sj=2040&jl=%E7%A6%8F%E5%B7%9E&p=',\
+            'android':'http://sou.zhaopin.com/jobs/searchresult.ashx?bj=160000&sj=2039&jl=%E7%A6%8F%E5%B7%9E&p=',\
+            'php':'http://sou.zhaopin.com/jobs/searchresult.ashx?bj=160000&sj=2041&jl=%E7%A6%8F%E5%B7%9E&p=',\
+            'web前端':'http://sou.zhaopin.com/jobs/searchresult.ashx?bj=160000&sj=864&jl=%E7%A6%8F%E5%B7%9E&p=',\
+            'ios':'http://sou.zhaopin.com/jobs/searchresult.ashx?bj=160000&sj=2038&jl=%E7%A6%8F%E5%B7%9E&p=',\
+            'C':'http://sou.zhaopin.com/jobs/searchresult.ashx?bj=160000&sj=2042&jl=%E7%A6%8F%E5%B7%9E&p=',\
+            '.net':'http://sou.zhaopin.com/jobs/searchresult.ashx?jl=%E7%A6%8F%E5%B7%9E&'\
+                    + 'kw=.net&isadv=0&isfilter=1&sg=3ed3b548f6cd4b41a75bb6f6cfe995d3&p='}
 
 headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3)\
             AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36'}
@@ -37,7 +38,7 @@ def jobFind(keyWord, cmpList, url, headers):
     result = soup.find_all(class_= 'gsmc')
     cmpCount = len(result)      #用于统计页面中的公司总数，如果低于某个设定数值，则到了末页，本值需要返回
     for companyDescribe in result:
-        time.sleep(0.1)
+        time.sleep(0.1)         # 兔子急了也咬人，天天爬人家网站，还是要讲道德的，感谢智联招聘提供的信息
         ###名称
         pattern1 = re.compile(r'k">.+公司')
         companyName = pattern1.search(str(companyDescribe))
@@ -62,7 +63,7 @@ def jobFind(keyWord, cmpList, url, headers):
     return cmpCount
 
 
-    ##针对职务寻找链接
+    ##针对职务寻找链接，暂时没什么用了
     # result = soup.find_all(class_= 'zwmc')
     # for jobDescribe in result:
     #     pattern = re.compile(r'http://jobs\.zhaopin\.com/.+\.htm')
@@ -100,32 +101,38 @@ def companyInfo(url, headers, data):
         text = i.strip().replace('\n', ' ')
         if text != '':
             data.append(text)
-    print(data[1])
-    time.sleep(6)
-    basic_info = geetest.query_detail(data[1], GSXT_HOST_FJ, GSXT_INDEX_FJ)
-    if basic_info is not None:
-        full_data = data + basic_info
-    else:
-        full_data = data + ['未查询到相关信息']
-    print('-'*20 + 'writer')
-    print(full_data)
-    writer.writerow(full_data)
+    writer.writerow(data)
+
+    # 以下代码用于查询企业工商信息，国家企业信用信息网福建省部分反爬虫太强，封IP很厉害，暂停使用
+    # print(data[1])
+    # time.sleep(6)
+    # basic_info = geetest.query_detail(data[1], GSXT_HOST_FJ, GSXT_INDEX_FJ)
+    # if basic_info is not None:
+    #     full_data = data + basic_info
+    # else:
+    #     full_data = data + ['未查询到相关信息']
+    # print('-'*20 + 'writer')
+    # print(full_data)
+    # writer.writerow(full_data)
 
 #主流程
-csvfile = open('csv_test.csv', 'w', encoding='utf-8-sig', newline='')
-writer = csv.writer(csvfile)
-titles = ["岗位类别","公司名称","链接","地址"\
-            ,"统一社会信用代码","企业名称","类型","法定代表人","注册资本","成立日期"\
-            ,"经营期限自","经营期限至","登记机关","核准日期","登记状态","住所","经营范围"]
-writer.writerow(titles)
-for key in urlDict.keys():
-    p = 0
-    cmpInPage = 30      # 页面内的公司数，用于判定是否末页
-    cmpList = []        # 同一个岗位已收集的公司名称，避免重复收集数据
-    while cmpInPage > 20:   #如果页面内公司数少于10，则判定为末页，否则继续下一页
-        p += 1
-        url1 = urlDict[key] + str(p)
-        print(key + '-' * 20 + str(p))
-        print(url1)
-        cmpInPage = jobFind(key, cmpList, url1, headers)
-csvfile.close()
+if __name__ == "__main__":
+    # 打开文件，写入字段名，目前前面4个有用
+    csvfile = open('csv_test.csv', 'w', encoding='utf-8-sig', newline='')
+    writer = csv.writer(csvfile)
+    titles = ["岗位类别","公司名称","链接","地址"\
+                ,"统一社会信用代码","企业名称","类型","法定代表人","注册资本","成立日期"\
+                ,"经营期限自","经营期限至","登记机关","核准日期","登记状态","住所","经营范围"]
+    writer.writerow(titles)
+    # 根据不同查询页面爬取数据
+    for key in urlDict.keys():
+        p = 0
+        cmpInPage = 30      # 页面内的公司数，用于判定是否末页
+        cmpList = []        # 同一个岗位已收集的公司名称，避免重复收集数据
+        while cmpInPage > 20:   #如果页面内公司数少于20，则判定为末页，否则继续下一页
+            p += 1
+            url1 = urlDict[key] + str(p)
+            print(key + '-' * 20 + str(p))
+            print(url1)
+            cmpInPage = jobFind(key, cmpList, url1, headers)
+    csvfile.close()         # 记得关闭文件
